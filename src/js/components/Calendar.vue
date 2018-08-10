2ebfd77eb97a955af138e82b08f1f412a3851ee8
Eli Kotyluk <eli@riboso.me>
2018-08-10 12:02:49 -0700

    Create days computed property for printing days of the month in Calendar.vue. Define moment object in index.js.

--- src/js/components/Calendar.vue
+++ src/js/components/Calendar.vue
@@ -1,15 +1,33 @@
-<template></template>
+<template>
+  <div>
+    <div v-for="day in days">{{ days }}</div>
+  </div>
+</template>
 
 
 <script>
-import moment from 'moment';
 
   export default {
     name: 'Calendar',
     data () {
       return {
-        month: moment(),
-        day: moment()
+        month: 9,
+        year: 2018
+      };
+    },
+    computed: {
+      // Create all days in current month.
+      days() {
+        // Array to push days to.
+        let days = [];
+        let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-MM');
+
+        do {
+          days.push(currentDay);
+          currentDay = this.$moment(currentDay).add(1, 'days');
+        } while (currentDay.month() + 1 === this.month);
+
+        return days;
       }
     }
   }
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,22 @@
 import Vue from 'vue';
 import App from './components/App.vue';
-import moment from 'moment';
 import moment from 'moment-timezone';
+
+// Set timezone to UTC.
+moment.tz.setDefault('UTC');
+
+// Create moment object to use in any component.
+Object.defineProperty(Vue.prototype, '$moment', {
+  get() {
+    return this.$root.moment
+  }
+});
 
 
 var app = new Vue({
   el: '#app',
+  data: {
+    moment
+  },
   render: h => h(App)
 });

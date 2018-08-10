7471a5f4d55f41704387aac39b1816d3fc12ba2b
Eli Kotyluk <eli@riboso.me>
2018-08-10 10:22:43 -0700

    Create Calendar component. Test moment package in Calendar.vue. Add Calendar component to App component.

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -1,8 +1,19 @@
 <template>
+  <div>
+    <calendar></calendar>
+  </div>
 </template>
 
 
 <script>
+import Calendar from './Calendar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Calendar
+  }
+}
 </script>
 
 
---
+++ src/js/components/Calendar.vue
@@ -0,0 +1,19 @@
+<template></template>
+
+
+<script>
+import moment from 'moment';
+
+  export default {
+    name: 'Calendar',
+    data () {
+      return {
+        month: moment(),
+        day: moment()
+      }
+    }
+  }
+</script>
+
+
+<style lang="scss"></style>

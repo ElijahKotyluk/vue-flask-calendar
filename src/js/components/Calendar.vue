350ca8df6c23c7e9c3a2f8b01fc2ff327bb10a47
Eli Kotyluk <eli@riboso.me>
2018-08-10 13:00:45 -0700

    Add if statement to correctly render padding days for the month of May.

--- src/js/components/Calendar.vue
+++ src/js/components/Calendar.vue
@@ -36,25 +36,31 @@
         // Reset currentDay value to first day of the month.
         currentDay = this.$moment(days[0]);
 
-        // Unshift currentDay to days array while currentDay is not Monday.
-        do {
-          // Instance of moment that decrements currentDay by 1 day.
-          currentDay = this.$moment(currentDay).subtract(1, 'days');
-          days.unshift(currentDay);
-          // 1 is Monday in the moment library.
-        } while (currentDay.day() !== 1);
+        // If statement to correctly render padding days for the month of May.
+        if (currentDay.day() !== 1) {
+          // Unshift currentDay to days array while currentDay is not Monday.
+          do {
+            // Instance of moment that decrements currentDay by 1 day.
+            currentDay = this.$moment(currentDay).subtract(1, 'days');
+            days.unshift(currentDay);
+            // 1 is Monday in the moment library.
+          } while (currentDay.day() !== 1);
+        }
 
 
         // Add padding days from the beginning of the following month.
         // Reset currentDay value to last day of the current month.
         currentDay = this.$moment(days.length - 1);
 
-        // Push currentDay to days array while currentDay is not Sunday.
-        do {
-          currentDay = this.$moment(currentDay).add(1, 'days');
-          days.push(currentDay);
+        // If statement to correctly render padding days for the month of May.
+        if (currentDay.day() !== 0) {
+          // Push currentDay to days array while currentDay is not Sunday.
+          do {
+            currentDay = this.$moment(currentDay).add(1, 'days');
+            days.push(currentDay);
           // 0 is Sunday in the moment library.
-        } while (currentDay.day() !== 0);
+          } while (currentDay.day() !== 0);
+      }
 
         return days;
       }
